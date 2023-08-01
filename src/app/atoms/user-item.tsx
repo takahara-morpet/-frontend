999571e562446d5fbc96b74f0579c4e660f59354
Yota Takahara <yota.takahara@techouse.jp>
2023-08-01 22:21:05 +0900

    update:as avatar

--- src/app/atoms/user-item.tsx
+++ src/app/atoms/user-item.tsx
@@ -1,25 +1,19 @@
 import React from 'react';
-import Image from 'next/image';
+import Avatar from '@mui/material/Avatar';
 import { ListItem as MuiListItem } from '@mui/material';
 
 interface ListUserItemProps{
-   
-    imageUri: string;
+    imageUri?: string; // Optionalに変更
     userName: string;
     statusMessage?: string;
-    width?: number;
-    height?: number;
-
+    size?: number; // heightとwidthをまとめてsizeに変更
 }
 
-
 export const ListUserItem: React.FC<ListUserItemProps> = ({
-    
     userName='Takahara',
     statusMessage='test profile',
-    imageUri='/sample/user.png',
-    height=100,
-    width=100 
+    imageUri='/sample/user.png', // default image
+    size=100, // sizeを追加
 }): JSX.Element => {
     return (
         <MuiListItem style={{
@@ -27,7 +21,11 @@ export const ListUserItem: React.FC<ListUserItemProps> = ({
             borderBottom: '1px solid #ccc',
             marginLeft: '3rem',
         }}>
-            {imageUri && <Image alt='ユーザー画像' src={imageUri} width={width} height={height} />}
+            <Avatar
+                src={imageUri}
+                alt='ユーザー画像'
+                style={{ width: size, height: size }} // Avatarを使って画像を丸く表示、サイズも指定
+            />
             <div style={{ 
                 marginLeft: '3rem', 
                 }}>
