baa964780c8682f1fd7766d82b1cde09f98202b8
Yota Takahara <yota.takahara@techouse.jp>
2023-08-04 00:40:27 +0900

    routing修正

---
+++ src/app/signup/form/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import SignupForm from "../../pages/signup-form"
+
+
+
+const SignupFormPage: React.FC = (): JSX.Element => {
+    return (
+        <SignupForm />
+    );
+}
+
+export default SignupFormPage;
