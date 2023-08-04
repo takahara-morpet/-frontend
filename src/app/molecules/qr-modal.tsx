30b2108e176ba13f91dcd1ff650bfdaa29b904c2
Najah7 <najahmatsuo7@gmail.com>
2023-08-04 21:56:33 +0900

    ✨ #34 [Add] QRを表示するためのモーダルを作成

---
+++ src/app/molecules/qr-modal.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import Image from "next/image";
+import Modal from '@mui/material/Modal';
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import exp from "constants";
+
+interface QRModalProps {
+  qrSrc: string;
+  handleOpen: () => void;
+  handleClose: () => void;
+  isOpen: boolean;
+}
+
+const QRModal: React.FC<QRModalProps> = ({
+    qrSrc,
+    // 下記を親でで定義してもらうか悩み中
+    handleClose,
+    isOpen
+}): JSX.Element => {
+    return(
+        <Modal
+        open={isOpen}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+        >
+        <Box sx={{
+            position: 'absolute',
+            top: '50%',
+            left: '50%',
+            transform: 'translate(-50%, -50%)',
+            width: 400,
+            bgcolor: 'background.paper',
+            border: '2px solid #000',
+            boxShadow: 24,
+            p: 4,
+        }}>
+            <Typography id="modal-modal-title" variant="h6" component="h2">
+            Your QR Code
+            </Typography>
+            <Image src={qrSrc} alt="QR Code" width={500} height={500} />
+        </Box>
+        </Modal>
+    )
+}
+
+export default QRModal;
