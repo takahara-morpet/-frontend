c362476286b06544da6a0474bb19791da1fc2d38
Yota Takahara <yota.takahara@techouse.jp>
2023-08-04 11:52:04 +0900

    検索bar忘れてた

--- src/app/users/page.tsx
+++ src/app/users/page.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import AddConnectionLayout from "../pages/users";
 import UsersList from "../pages/users";
-
+import SearchBar from "../molecules/search-bar";
 
 const UsersPage: React.FC = (): JSX.Element => {
     return (
+        <div>
+
+       <SearchBar />
         <UsersList />
+         </div>
     );
 }
 
