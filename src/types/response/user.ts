5b2cf92d63bd6aeca599f07efd81f2ec4f71b324
Najah7 <najahmatsuo7@gmail.com>
2023-08-05 22:21:39 +0900

    🩹[Fix] Userの型を修正

--- src/types/response/user.ts
+++ src/types/response/user.ts
@@ -6,9 +6,6 @@ export type User = {
     first_kan?: string;
     icon_url?: string;
     status_text?: string;
-  };
-  
-export type UserProfile = {
     background_url?: string;
     intro_text?: string;
     private_memo?: string;
@@ -16,4 +13,4 @@ export type UserProfile = {
     use_discord?: boolean;
     use_twitter?: boolean;
     use_github?: boolean;
-  };
+};
