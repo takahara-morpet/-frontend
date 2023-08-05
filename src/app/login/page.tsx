c4cd8440208a3afdabf48f64e05b1ea2c7a5538c
Najah7 <najahmatsuo7@gmail.com>
2023-08-06 06:50:57 +0900

    ♻️ pagesのリファクタリング

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,18 +1,35 @@
 import React from "react";
 
 import { Grid, Container, Typography } from "@mui/material";
+import Box from "@mui/material/Box";
 
 import UseableButton from "@/components/molecules/useable-button";
 
+
 const Login: React.FC = (): JSX.Element => {
-  return (
-    <Container maxWidth="xs" sx={{
+
+  const styles = {
+    container: {
       marginTop: 8,
       display: 'flex',
       flexDirection: 'column',
       alignItems: 'center',
       height: '70vh',
-    }}>
+    },
+    gridContainer: {
+      minHeight: '70vh',
+    },
+    textContainer: {
+      textAlign: 'center',
+      margin: 20
+    },
+    textSpan: {
+      color: 'rgb(0, 112, 210)'
+    },
+  }
+
+  return (
+    <Container maxWidth="xs" sx={styles.container}>
       <Typography component='h1' variant='h4'>
         新規登録
       </Typography>
@@ -22,7 +39,7 @@ const Login: React.FC = (): JSX.Element => {
         direction="column"
         alignItems="center"
         justifyContent="center"
-        style={{ minHeight: "70vh" }}
+        style={styles.gridContainer}
       >
         <Grid item>
           <img src="/penguin.svg" alt="penguin" />
@@ -56,12 +73,12 @@ const Login: React.FC = (): JSX.Element => {
           />
         </Grid>
       </Grid>
-      <div style={{ textAlign: 'center', margin: 20 }}>
+      <Box sx={styles.textContainer}>
         アカウントをお持ちでないですか？
-        <span style={{ color: 'rgb(0, 112, 210)' }}>
+        <span style={styles.textSpan}>
           新規登録
         </span>
-      </div>
+      </Box>
     </Container>
   );
 };
