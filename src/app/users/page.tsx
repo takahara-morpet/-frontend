633227e87228bc2381beb67586669a959f39aa8d
Yota Takahara <yota.takahara@techouse.jp>
2023-08-05 13:56:04 +0900

    header and footer added

--- src/app/users/page.tsx
+++ src/app/users/page.tsx
@@ -2,13 +2,17 @@ import React from "react";
 import AddConnectionLayout from "../template/users";
 import UsersList from "../template/users";
 import SearchBar from "../molecules/search-bar";
+import Footer from "../organisms/footer";
+import Header from "../organisms/header";
 
 const UsersPage: React.FC = (): JSX.Element => {
     return (
         <div>
 
+        <Header title="ユーザー一覧" />
        <SearchBar />
         <UsersList />
+        <Footer />
          </div>
     );
 }
