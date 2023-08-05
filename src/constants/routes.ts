ab10fea37521c0b0ce9711a4bbcc873dce85d8d9
Yota Takahara <yota.takahara@techouse.jp>
2023-08-05 22:49:24 +0900

    even

--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -1,8 +1,11 @@
 enum Routes {
     HOME_URL = '/',
     USERS_URL = '/users',
+    EVENTS_URL = '/events',
     Profile_URL = '/accounts', // TODO: 仮
-    HOME_USERS_PATH=`http://localhost:4000${USERS_URL}`
+    HOME_USERS_PATH=`http://localhost:4000${USERS_URL}`,
+    HOME_EVENTS_PATH=`http://localhost:4000${EVENTS_URL}`,
+
 }
 
 export default Routes;
