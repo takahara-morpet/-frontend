94fd55d9f22c4802b5fbb1778061552be98afa81
Yota Takahara <yota.takahara@techouse.jp>
2023-08-05 17:16:15 +0900

    変更

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Image from 'next/image'; // Imageをインポート
+import Image from 'next/image';
 import AddConnectionLayout from "../template/events";
 import EventsList from "../template/events";
 import SearchBar from "../molecules/search-bar";
@@ -8,28 +8,27 @@ import Header from "../organisms/header";
 import AddIcon from "@mui/icons-material/Add";
 import Fab from "@mui/material/Fab";
 
-
 const EventsPage: React.FC = (): JSX.Element => {
-    return (
-        <div>
-            <Header title="イベント一覧" />
-            <div style={{ marginTop: '20px' }}>
-                <SearchBar />
-            </div>
-            <EventsList />
-            <div style={{
-                position: 'fixed', // 'fixed'を指定して固定表示
-                right: '40px', // 右側から20pxの位置
-                bottom: '100px', // 下側から20pxの位置
-            }}>
-            <Fab color="primary" aria-label="add">
-                <AddIcon />
-            </Fab>
-        
-            </div>
-            <Footer />
+  return (
+   
+      <div>
+        <Header title="イベント一覧" />
+        <div style={{ marginTop: '20px' }}>
+          <SearchBar />
         </div>
-    );
+        <EventsList />
+        <div style={{
+          position: 'fixed',
+          right: '40px',
+          bottom: '100px',
+        }}>
+        <Image alt="イベント作成" src="create_event.svg" width={70} height={70} />
+         
+        </div>
+        <Footer />
+      </div>
+   
+  );
 }
 
 export default EventsPage;
