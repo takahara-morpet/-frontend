a0e2a8ad52bb08f7088ebe74fbeec1bbaff1f584
Najah7 <najahmatsuo7@gmail.com>
2023-08-06 14:47:25 +0900

    🩹 [Fix] エンドポイントを修正

--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -2,9 +2,13 @@ import getEndpoint from '@/lib/router/url';
 
 export const DEV_SERVER_URL = "http://localhost:4000"
 export const PRO_SERVER_URL = "https://graph.shion.pro/api/v0"
+
 export const HOME_URL = `${getEndpoint()}/events`
-export const USERS_URL = `${getEndpoint()}/friends`
-export const USERS_DETAIL_URL = `${getEndpoint()}/user-detail`
-export const EVENTS_URL = `${getEndpoint()}/events`
-export const EVENTS_DETAIL_URL = `${getEndpoint()}/event-detail`
 export const PROFILE_URL = `${getEndpoint()}/user/@me`
+export const USERS_DETAIL_URL = `${getEndpoint()}/user`
+export const USERS_URL = `${getEndpoint()}/friends`
+export const EVENTS_URL = `${getEndpoint()}/events`
+export const EVENTS_DETAIL_URL = `${getEndpoint()}/event/`
+export const SEARCH_FRIENDS_URL = `${getEndpoint()}/search/friends`
+
+
